5bf525598fb5da47aaf07ad21f10efdafc9501a9
devwithdonald <hend.donald@gmail.com>
2019-06-24 14:17:31 -0400

    Employees can now login!

--- src/main/java/com/donald/dao/EmployeeDAOImpl.java
+++ src/main/java/com/donald/dao/EmployeeDAOImpl.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import com.donald.pojos.Associate;
 import com.donald.pojos.Employee;
+import com.donald.pojos.Supervisor;
 import com.donald.util.ConnectionFactory;
 import com.donald.util.LoggingUtil;
 
@@ -40,9 +41,15 @@ public class EmployeeDAOImpl implements EmployeeDAOInt {
 			while (rs.next()) {
 				//if the employee in the row is an associate
 				// 1 -> associate ID in database
+				
+				//should be switch() case
 				if (rs.getInt("employee_type_id") == 1) {
 					employee = new Associate(rs.getInt("employee_id"), rs.getString("username"), rs.getString("password"), rs.getString("employee_type"));
 				} else if (rs.getInt("employee_type_id") == 2) {
+					employee = new Supervisor(rs.getInt("employee_id"), rs.getString("username"), rs.getString("password"), rs.getString("employee_type"));
+				} else if (rs.getInt("employee_type_id") == 3) {
+					employee = new Supervisor(rs.getInt("employee_id"), rs.getString("username"), rs.getString("password"), rs.getString("employee_type"));
+				} else if (rs.getInt("employee_type_id") == 4) {
 					employee = new Supervisor(rs.getInt("employee_id"), rs.getString("username"), rs.getString("password"), rs.getString("employee_type"));
 				}
 				
