974fdc81b3d8c34f8be099ab072df5ec228b3658
devwithdonald <hend.donald@gmail.com>
2019-06-28 23:55:52 -0400

    front end done for updating requests

---
+++ src/main/webapp/JavaScript/UpdateRequests.js
@@ -0,0 +1,42 @@
+function getRequests(){
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState === 4 & xhr.status === 200){
+            let requestArr = JSON.parse(xhr.responseText);
+            appendToTable(requestArr);     
+        }
+    }
+
+    xhr.open("GET", "UpdateRequestsServlet", true);
+
+    xhr.send();
+}
+
+function appendToTable(requestArr){
+    const tableBody = document.getElementById('update-view-table-body');
+
+    //clear contents first!
+    tableBody.innerHTML = "";
+
+    for (let i = 0; i < requestArr.length; i++){
+        tableBody.innerHTML += `
+        <tr>
+            <td>${requestArr[i].id}</td>
+            <td>${requestArr[i].eventType}</td>
+            <td>${requestArr[i].dateOfEvent}</td>
+            <td>${requestArr[i].description}</td>
+            <td>${requestArr[i].gradingFormat}</td>
+            <td>${requestArr[i].passingGrade}</td>            
+        </tr>
+        `;
+    }
+    
+}
+
+
+window.onload = function () {
+    getRequests();
+
+  //document.getElementById("approvalForm").addEventListener("submit", updateRequest)
+}
