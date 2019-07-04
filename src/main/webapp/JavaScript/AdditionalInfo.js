b15713e3d774a7453524ed6d76cbcaa665423cca
devwithdonald <hend.donald@gmail.com>
2019-07-04 19:53:24 -0400

    POST ajax call for response for additional info is complete

--- src/main/webapp/JavaScript/AdditionalInfo.js
+++ src/main/webapp/JavaScript/AdditionalInfo.js
@@ -33,6 +33,44 @@ function appendToTable(requestArr){
     
 }
 
+function response() {
+    event.preventDefault();
+
+    let requestId = document.getElementById("request_id").value;
+    let responseMessage = document.getElementById("response").value;
+
+
+    //make new JSON object
+    let response = new Response(requestId, responseMessage);
+
+    let xhr = new XMLHttpRequest();
+
+     
+	xhr.onreadystatechange = function() {
+        
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            
+            alert(xhr.responseText);
+            getRequests();
+
+		}
+    }
+    
+    xhr.open("POST", "./AdditionalInfoServlet", true);
+
+
+    xhr.send(JSON.stringify(response));
+
+
+}
+
+let ReponseDecision = function (requestId, responseMessage){
+    this.requestId = requestId;
+    this.responseMessage = responseMessage;
+}
+
 window.onload = function () {
     getRequests()
+
+    document.getElementById("responseForm").addEventListener("submit", response)
 }
