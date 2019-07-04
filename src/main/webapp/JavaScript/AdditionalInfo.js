08570449c1807150520444c56fcf40e1b831ba73
devwithdonald <hend.donald@gmail.com>
2019-07-04 19:12:56 -0400

    front end and get request done for responding to the additional information

---
+++ src/main/webapp/JavaScript/AdditionalInfo.js
@@ -0,0 +1,38 @@
+function getRequests() {
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState === 4 & xhr.status === 200){
+            let requestArr = JSON.parse(xhr.responseText);
+            appendToTable(requestArr);     
+        }
+    }
+
+    xhr.open("GET", "./AdditionalInfoServlet", true);
+
+    xhr.send();
+}
+
+function appendToTable(requestArr){
+    const tableBody = document.getElementById('info-view-table-body');
+
+    //clear contents first!
+    tableBody.innerHTML = "";
+
+    for (let i = 0; i < requestArr.length; i++){
+        tableBody.innerHTML += `
+        <tr>
+            <td>${requestArr[i].id}</td>
+            <td>${requestArr[i].additionalInfo}</td>
+            <td>${requestArr[i].eventType}</td>
+            <td>${requestArr[i].description}</td>
+            <td>${requestArr[i].gradingFormat}</td>        
+        </tr>
+        `;
+    }
+    
+}
+
+window.onload = function () {
+    getRequests()
+}
